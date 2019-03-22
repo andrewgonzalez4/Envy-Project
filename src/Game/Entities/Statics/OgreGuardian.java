aeedf461135917f7c2a99865ffe0dfd306b52d53
andrew.gonzalez4 <andrew.gonzalez4@192.168.0.2>
2019-03-22 11:59:05 -0400

    Merged Quest and Ogre changes from my partner and I.

--- src/Game/Entities/Statics/OgreGuardian.java
+++ src/Game/Entities/Statics/OgreGuardian.java
@@ -36,12 +36,14 @@ public class OgreGuardian extends BaseStaticEntity{
 	@Override
 	public void render(Graphics g) {
 		
-		if(handler.getEntityManager().getPlayer().questComplete = false) {
+		if(handler.getEntityManager().getPlayer().questComplete == false) {
 			g.drawImage(stop.getCurrentFrame(), (int)(handler.getXDisplacement() + xPosition),(int)( handler.getYDisplacement() + yPosition), width, height, null);
 			collision = new Rectangle((int)(handler.getXDisplacement() + xPosition + 35), (int)(handler.getYDisplacement() + yPosition + 50), width/4, height/2);
 		}
 		
 		else {
+			
+			
 			
 		}
 		
